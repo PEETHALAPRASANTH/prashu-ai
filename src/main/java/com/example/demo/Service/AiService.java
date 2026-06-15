08eff14af54348a28153c066b1de5a80d240f394
BalajiYarraguntla <baluadvik22@gmail.com>
2026-06-15 18:30:03 +0530

    Updated model to GPT-4.1 mini

--- src/main/java/com/example/demo/Service/AiService.java
+++ src/main/java/com/example/demo/Service/AiService.java
@@ -34,7 +34,8 @@ public class AiService {
         userMessageObj.setContent(userMessage);
 
         OpenAIRequest request = new OpenAIRequest();
-        request.setModel("openai/gpt-4.1-mini");
+        request.setModel("request.setModel(\"openai/gpt-4.1-mini\");");
+        
         request.setMessages(List.of(systemMessage, userMessageObj));
 
         HttpHeaders headers = new HttpHeaders();
