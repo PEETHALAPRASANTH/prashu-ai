b75c1f52778aa536396119c6052bb476de4843a9
BalajiYarraguntla <baluadvik22@gmail.com>
2026-06-15 18:40:05 +0530

    updated version

--- src/main/java/com/example/demo/Service/AiService.java
+++ src/main/java/com/example/demo/Service/AiService.java
@@ -34,7 +34,7 @@ public class AiService {
         userMessageObj.setContent(userMessage);
 
         OpenAIRequest request = new OpenAIRequest();
-        request.setModel("request.setModel(\"openai/gpt-4.1-mini\");");
+        request.setModel("openai/gpt-4.1-mini");
         
         request.setMessages(List.of(systemMessage, userMessageObj));
 
